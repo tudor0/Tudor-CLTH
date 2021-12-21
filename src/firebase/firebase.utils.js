bae1065ed5c36154187bac8948c6c8b4346c3393
Tudor <tudor.tuglea@gmail.com>
2021-12-21 12:19:49 +0200

    Implemented google and email authentication

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -14,7 +14,6 @@ firebase.initializeApp(firebaseConfig);
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
-console.log("meow", firestore.collection("users"));
 const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({ prompt: "select_account" });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
