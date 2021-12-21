023cd81d180fb9de6839f3f5c8cdbfaa3b0212c3
Tudor <tudor.tuglea@gmail.com>
2021-12-21 11:56:38 +0200

    Almost done with email auth

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import ShopPage from "./pages/shop/shop.component";
 import HomePage from "./pages/homepage/homepage.component";
 import Header from "./components/header/header.component";
 import SignInSignUp from "./pages/sign-in-sign-up/sign-in-sign-up.component";
-import { auth } from "./firebase/firebase.utils";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
 class App extends React.Component {
   constructor() {
@@ -16,9 +16,19 @@ class App extends React.Component {
   }
   unsubscribeFromAuth = null;
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
-      this.setState({ currentUser: user });
-      console.log(user);
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+        userRef.onSnapshot((snapShot) => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data(),
+            },
+          });
+          console.log(this.state);
+        });
+      } else this.setState({ currentUser: userAuth });
     });
   }
   componentWillUnmount() {
--- src/pages/sign-in-sign-up/sign-in-sign-up.component.jsx
+++ src/pages/sign-in-sign-up/sign-in-sign-up.component.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./sign-in-sign-up.styles.scss";
 import SignIn from "../../components/signIn/sign-in.component";
+import SignUp from "../../components/sign-up/sign-up.component";
 const SignInSignUp = () => {
   return (
-    <>
+    <div className="sign-in-and-sign-up">
       <SignIn />
-    </>
+      <SignUp />
+    </div>
   );
 };
 export default SignInSignUp;
